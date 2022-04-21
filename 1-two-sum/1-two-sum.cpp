a8192116846a2b1bc147a55a3700b868a970ef9b
Exitialium <henrywu0424@gmail.com>
2022-04-20 22:13:46 -0400

    Time: 15 ms (72.18%), Space: 10.9 MB (44.74%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        for(int i = 0; i< nums.size(); i++) {
-            for(int j = i + 1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+        unordered_map <int,int> visited ;
+        for (int i = 0; i < nums.size(); i++ ) {
+            if (visited.find(target - nums[i]) == visited.end()) {
+                visited[nums[i]] = i;
+            } else {
+                ans.push_back(visited[target - nums[i]]);
+                ans.push_back(i);
+                break;
             }
         }
         return ans;
